5f71043969b96efed508e43696f9566c381a42f9
R.Andres Castaneda <acastanedaa@worldbank.org>
2026-03-16 11:04:59 -0400

    add spl<>20260324_2021_01_02_PROD<>2026-03-16 11:04:59

--- R/setup.R
+++ R/setup.R
@@ -22,20 +22,17 @@ if (!"lkups" %in% ls() || isTRUE(force)) {
     fs::path()
   fs::dir_ls(data_dir, recurse = FALSE)
 
+  version <- "20250930_2017_01_02_PROD"
+  version <- "20250930_2021_01_02_PROD"
 
-version  <- "20240627_2017_01_02_PROD"
-version  <- "20240627_2011_02_02_PROD"
+  version <- "20260324_2017_01_02_PROD"
+  version <- "20260324_2021_01_02_PROD"
 
-version  <- "20250401_2017_01_02_PROD"
-version  <- "20250401_2021_01_02_PROD"
-
-version  <- "20250930_2017_01_02_PROD"
-version  <- "20250930_2021_01_02_PROD"
-
-
-## Lkup files ===========
-lkups <- pipapi::create_versioned_lkups(data_dir        = data_dir,
-                                        vintage_pattern = version)
+  ## Lkup files ===========
+  lkups <- pipapi::create_versioned_lkups(
+    data_dir = data_dir,
+    vintage_pattern = version
+  )
 }
 
 # ppp years
@@ -43,13 +40,12 @@ ppp_year <- py <- version |>
   gsub("(.*)_([0-9]{4})(_.*)", "\\2", x = _) |>
   as.numeric()
 # lkup <-  lkups$versions_paths$`20230328_2011_02_02_PROD`
-lkup <-  lkups$versions_paths[[lkups$latest_release]]
+lkup <- lkups$versions_paths[[lkups$latest_release]]
 
 
 new_dir <-
   fs::path("p:/03.pip/estimates/1kbins_lineup", version) |>
   fs::dir_create()
-
 
 
 ## filter data --------------
@@ -62,6 +58,7 @@ yr <- NULL
 ## TFS dir -----------
 
 gls <- pipfun::pip_create_globals(
-  root_dir   = Sys.getenv("PIP_ROOT_DIR"),
-  vintage    = version,
-  create_dir = FALSE)
+  root_dir = Sys.getenv("PIP_ROOT_DIR"),
+  vintage = version,
+  create_dir = FALSE
+)
